f0acf35e3e8e8673a24d8154dd2a26a78853c47b
Paulshapwala <www.wolverine99999@gmail.com>
2023-03-30 09:52:20 +0300

    increased beeping time

--- functions.py
+++ functions.py
@@ -26,7 +26,7 @@ class AlarmClock:
             print()
             print("Wake up!")
             frequency = 2500  # Set Frequency To 2500 Hertz
-            duration = 1000  # Set Duration To 1000 ms == 1 second
+            duration = 10000  # Set Duration To 1000 ms == 1 second
             winsound.Beep(frequency, duration)
             return False
         else:
